9d8351ab66d3160edab28f66348a8b236701e46b
Rodrez <fabian.rodrez@gmail.com>
2021-02-15 22:31:51 -0500

    Update indeed.py

--- jobpy/jobpy_scrapy/jobpy_scrapy/spiders/indeed.py
+++ jobpy/jobpy_scrapy/jobpy_scrapy/spiders/indeed.py
@@ -1,11 +1,36 @@
-# -*- coding: utf-8 -*-
 import scrapy
+from scrapy_selenium import SeleniumRequest 
 
 
 class IndeedSpider(scrapy.Spider):
     name = 'indeed'
-    allowed_domains = ['www.indeed.com/jobs?q=software+engineer+intern&l=United+States']
-    start_urls = ['http://www.indeed.com/jobs?q=software+engineer+intern&l=United+States/']
+    allowed_domains = ['https://www.indeed.com/jobs?q=software+engineer&l=Mechanicsburg%2C+PA']
+    start_urls = ['http://https://www.indeed.com/jobs?q=software+engineer&l=Mechanicsburg%2C+PA/']
+
+    def start_requests(self): 
+        yield SeleniumRequest( 
+            url ='https://www.indeed.com/jobs?q=software+engineer&l=Mechanicsburg%2C+PA&sort=date', 
+            wait_time = 3, 
+            screenshot = True, 
+            callback = self.parse,  
+            dont_filter = True    
+        ) 
 
     def parse(self, response):
-        pass
+        
+        jobs = response.css('.jobsearch-SerpJobCard')
+   
+
+        for job in jobs:
+            title = job.css('.jobtitle').attrib['title']
+            company = job.css('.company').css('a::text').get()
+            location = job.css('.location::text').get()
+            apply_link = job.css('.jobtitle').attrib['href']
+
+
+            yield {
+                "Job Title": title,
+                "Company": company,
+                "Location": location,
+                "Apply": "https://www.indeed.com" + apply_link,
+            }
